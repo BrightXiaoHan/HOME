7d2f100fca68f784bd7a94c8e6f2714662b3cf33
BrightXiaoHan <beatmight@gmail.com>
2023-04-17 23:58:01 +0800

    update

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -2,4 +2,4 @@ DIR=~/.cache/homecli
 
 conda-pack -o $DIR/miniconda.tar.gz
 
-tar -zcvf $DIR/homecli.tar.gz $DIR/homecli $DIR/nvim --exclude=$DIR/homecli/miniconda
+tar -zcvf $DIR/homecli.tar.gz ~/.cache/homecli ~/.cache/nvim --exclude=$DIR/miniconda

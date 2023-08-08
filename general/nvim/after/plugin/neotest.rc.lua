9fa618a94a6b69f11a99b7b78ded5e07aab1eb2a
BrightXiaoHan <beatmight@gmail.com>
2023-08-08 13:40:07 +0800

    fix neotest python path

--- general/nvim/after/plugin/neotest.rc.lua
+++ general/nvim/after/plugin/neotest.rc.lua
@@ -4,6 +4,10 @@ if (not status) then return end
 local status, neotest_python = pcall(require, "neotest-python")
 if (not status) then return end
 
+-- if windows use .venv/Scripts/python.exe
+-- else use .venv/bin/python
+-- local python = vim.fn.has("win32") == 1 and ".venv\\Scripts\\python.exe" or ".venv/bin/python"
+
 require("neotest").setup({
     adapters = {
       require("neotest-python")({
@@ -21,7 +25,7 @@ require("neotest").setup({
           -- Can also be a function to return dynamic value.
           -- If not provided, the path will be inferred by checking for 
           -- virtual envs in the local directory and for Pipenev/Poetry configs
-          python = ".venv/bin/python",
+          python = "python",
           -- Returns if a given file path is a test file.
           -- NB: This function is called a lot so don't perform any heavy tasks within it.
           is_test_file = function(file_path)

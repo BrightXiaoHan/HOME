16f4752c5092470419e8032920f4d41fb1dbbef3
BrightXiaoHan <beatmight@gmail.com>
2023-08-12 03:46:51 +0000

    fix pack destination

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -3,15 +3,15 @@ DIR=~/.cache/homecli
 source $DIR/miniconda/bin/activate
 $DIR/miniconda/bin/conda-pack -o $DIR/miniconda.tar.gz > /dev/null
 
-PWD=$(pwd)
-echo $PWD
+CURDIR=$(pwd)
+echo $CURDIR
 
 cd $DIR
-tar -cvf $PWD/homecli.tar \
+tar -cvf $CURDIR/homecli.tar \
     HOME bin miniconda.tar.gz nodejs > /dev/null
 rm miniconda.tar.gz
 cd -
 
 cd ~/.local/share/
-tar -rvf $PWD/homecli.tar nvim/ > /dev/null
+tar -rvf $CURDIR/homecli.tar nvim/ > /dev/null
 cd -

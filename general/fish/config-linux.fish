b2441744a8ff09d9422d67e81359d34610fa46e4
BrightXiaoHan <beatmight@gmail.com>
2023-09-06 16:24:53 +0800

    fix pyenv config

--- general/fish/config-linux.fish
+++ general/fish/config-linux.fish
@@ -13,7 +13,15 @@ set -gx MAMBA_ROOT_PREFIX ~/.cache/homecli/miniconda
 # pyenv
 set -gx PYENV_ROOT ~/.cache/homecli/pyenv
 set -gx PATH $PYENV_ROOT/bin $PATH
-command -v pyenv >/dev/null || eval "$(pyenv init -)"
+# if pyenv exists, initialize it
+if test -f $PYENV_ROOT/bin/pyenv
+    pyenv init - | source
+end
+
+
+set -gx CPPFLAGS "-I$CONDA_PREFIX/include " $CPPFLAGS
+set -gx LDFLAGS "-L$CONDA_PREFIX/lib " $LDFLAGS
+set -gx CONFIGURE_OPTS "-with-openssl=$CONDA_PREFIX " $CONFIGURE_OPTS
 
 if not nvim --headless -c quit > /dev/null 2>&1
     alias nvim='nvim --appimage-extract-and-run'

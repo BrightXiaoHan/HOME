1a86562fbe8df5d4d6b5519bf3d03f7f5e34ff60
BrightXiaoHan <beatmight@gmail.com>
2023-11-03 07:44:36 +0000

    shift if $1 is set

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -3,13 +3,16 @@ set -e
 DIR=${HOMECLI_INSTALL_DIR:-$HOME/.homecli}
 OUTFILE=${1:-homecli.tar.gz}
 
+# shift if $1 is set
+if [ -n "$1" ]; then
+	shift
+fi
 source $DIR/miniconda/bin/activate
 $DIR/miniconda/bin/conda-pack -o $DIR/miniconda.tar.gz
 
 CURDIR=$(pwd)
 echo $CURDIR
 
-OUTFILE=${1:-homecli.tar.gz}
 cd $DIR
 tar -cvf $CURDIR/$OUTFILE \
 	HOME bin miniconda.tar.gz pyenv pipx nvim

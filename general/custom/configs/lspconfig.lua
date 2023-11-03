30a58fc420de6dd2f6ed1eb07a12d601fca3a7bd
BrightXiaoHan <beatmight@gmail.com>
2023-11-03 11:02:29 +0800

    disable pyright diagnostic

--- general/custom/configs/lspconfig.lua
+++ general/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "lua_ls", "bashls"}
+local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "lua_ls", "bashls" }
 
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
@@ -14,4 +14,11 @@ for _, lsp in ipairs(servers) do
 end
 
 --
--- lspconfig.pyright.setup { blabla}
+lspconfig.pyright.setup({
+	-- disable diagnostics
+	settings = {
+		python = {
+			analysis = { diagnosticMode = "off", typeCheckingMode = "off" },
+		},
+	},
+})

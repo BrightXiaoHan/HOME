53ea61d64baeaa8d9f41ede57449a7ae3894ead6
BrightXiaoHan <beatmight@gmail.com>
2024-12-26 15:54:08 +0800

    add start-proxy command

--- general/fish/config.fish
+++ general/fish/config.fish
@@ -23,29 +23,36 @@ set -gx CRYPTOGRAPHY_OPENSSL_NO_LEGACY 1
 # NodeJS
 set -gx PATH node_modules/.bin $PATH
 
+function start-proxy
+    shadowsocksr-cli --setting-url https://xxjcdy.cfd/link/suKDLaU0nUv6Ye1d
+    shadowsocksr-cli -u
+    shadowsocksr-cli --fast-node
+    shadowsocksr-cli --http-proxy start
+end
+
 # proxy alias
 alias setproxy="set ALL_PROXY 'socks5://127.0.0.1:1080'"
 alias unsetproxy="set -e ALL_PROXY"
 alias ip="curl http://ip-api.com/json/?lang=zh-CN"
-alias sethttpproxy="set HTTPS_PROXY 'http://127.0.0.1:7890'"
+alias sethttpproxy="set -gx HTTPS_PROXY 'http://127.0.0.1:7890'"
 alias unsethttpproxy="set -e HTTPS_PROXY"
 
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
-  source $LOCAL_CONFIG
+    source $LOCAL_CONFIG
 end
 
 switch (uname)
-  case Darwin
-    source (dirname (status --current-filename))/config-osx.fish
-  case Linux
-    source (dirname (status --current-filename))/config-linux.fish
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
 end
 
 # if starship and zoxide installed, init them
-if type starship > /dev/null 2>&1
-  starship init fish | source
+if type starship >/dev/null 2>&1
+    starship init fish | source
 end
-if type zoxide > /dev/null 2>&1
-  zoxide init fish | source
+if type zoxide >/dev/null 2>&1
+    zoxide init fish | source
 end

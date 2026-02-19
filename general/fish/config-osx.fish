97222dbcf1f3f4a9f621aec9363ceb451c81d403
BrightXiaoHan <beatmight@gmail.com>
2026-02-19 09:12:43 +0800

    fix(fish): correct command substitution comment in config-osx.fish

--- general/fish/config-osx.fish
+++ general/fish/config-osx.fish
@@ -1,7 +1,7 @@
 # Ensure Homebrew binaries are available
 fish_add_path --path /opt/homebrew/bin
 
-set -gx MAKE_OPTS -j(sysctl -n hw.ncpu) # set MAKE_OPTS to number of cores minus 1
+set -gx MAKE_OPTS -j(sysctl -n hw.ncpu) # set MAKE_OPTS to number of cores
 set -gx LANG en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
 

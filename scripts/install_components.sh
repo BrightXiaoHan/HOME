9e27ecbae8da38d0282733cbac8f007af9f8cc5c
BrightXiaoHan <beatmight@gmail.com>
2026-05-24 14:05:46 +0800

    Select frp release asset by architecture

--- scripts/install_components.sh
+++ scripts/install_components.sh
@@ -323,6 +323,26 @@ homecli_select_mihomo_asset() {
 	printf '%s\t%s\n' "$best_name" "$best_url"
 }
 
+homecli_select_frp_asset() {
+	local assets_file=$1
+	local wanted_arch name url
+
+	case "$HOMECLI_ARCH" in
+	x86_64 | amd64) wanted_arch=amd64 ;;
+	aarch64 | arm64) wanted_arch=arm64 ;;
+	*) return 1 ;;
+	esac
+
+	while IFS=$'\t' read -r name url; do
+		[ -n "$name" ] || continue
+		[[ "$name" == frp_*_linux_"$wanted_arch".tar.gz ]] || continue
+		printf '%s\t%s\n' "$name" "$url"
+		return 0
+	done <"$assets_file"
+
+	return 1
+}
+
 homecli_is_musl() {
 	if ! command -v ldd >/dev/null 2>&1; then
 		return 1
@@ -581,23 +601,22 @@ homecli_install_trzsz() {
 
 homecli_install_frp() {
 	local overwrite=${1:-true}
-	local latest_version url archive_name extracted_dir tmp binary
-
-	latest_version=$(homecli_latest_release_tag fatedier frp)
-	if [ "$HOMECLI_ARCH" = "x86_64" ] || [ "$HOMECLI_ARCH" = "amd64" ]; then
-		url="https://github.com/fatedier/frp/releases/download/$latest_version/frp_${latest_version#v}_linux_amd64.tar.gz"
-	else
-		url="https://github.com/fatedier/frp/releases/download/$latest_version/frp_${latest_version#v}_linux_arm64.tar.gz"
-	fi
+	local assets_file asset archive_name url extracted_dir tmp binary
 
 	homecli_log "Installing frp..."
 	if [ -e "$HOMECLI_BIN_DIR/frpc" ] && [ "$overwrite" != "true" ]; then
 		return 0
 	fi
 
+	homecli_temp_file assets_file
+	homecli_release_assets_to_file fatedier frp "$assets_file"
+	if ! asset=$(homecli_select_frp_asset "$assets_file"); then
+		homecli_die "Unable to find a suitable frp release asset"
+	fi
+	IFS=$'\t' read -r archive_name url <<<"$asset"
+
 	homecli_temp_file tmp
 	homecli_curl "$tmp" "$url"
-	archive_name=${url##*/}
 	extracted_dir="$HOMECLI_CACHE_DIR/${archive_name%.tar.gz}"
 	rm -rf "$extracted_dir"
 	tar -xzf "$tmp" -C "$HOMECLI_CACHE_DIR"
